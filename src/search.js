1ccb83243e21a84c2afd47f0eb492698cd1d41a9
Mila <milawp27@gmail.com>
2022-07-07 18:49:41 +0200

    added documentation url for api

--- src/search.js
+++ src/search.js
@@ -7,6 +7,7 @@ export default function Search() {
 
   function handleSearch(event) {
     event.preventDefault();
+    //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
