c64c2cf130c7e2e23d562c4c3749a0b4e2b1518f
Mila <milawp27@gmail.com>
2022-07-07 18:48:40 +0200

    added axios

--- src/search.js
+++ src/search.js
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
 import "./search.css";
+import axios from "axios";
 
 export default function Search() {
   let [keyword, setKeyword] = useState("");
 
   function handleSearch(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
+  //https://api.dictionaryapi.dev/api/v2/entries/en_US/lime
 
   return (
     <div className="search">
