eb76a110354125e54fa8e255ca2023ae3cccdb81
Mila <milawp27@gmail.com>
2022-07-08 13:37:53 +0200

    Rendered Meanings
    Meanings, examples, and results.

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
     return (
       <div className="Results">
-        <h2 className="Name">{props.results.word}</h2>{" "}
+        <h2 className="Name text-capitalize">{props.results.word}</h2>{" "}
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
       </div>
     );
   } else {
